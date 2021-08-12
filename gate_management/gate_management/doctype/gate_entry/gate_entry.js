6c1277817a641ef52babe6d34cabc0269c1b88f2
root <laxmantandon@gmail.com>
2021-08-12 13:56:38 +0000

    initial commit

---
+++ gate_management/gate_management/doctype/gate_entry/gate_entry.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, laxmantandon@gmail.com and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Gate Entry', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ gate_management/gate_management/doctype/gate_entry/test_gate_entry.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2021, laxmantandon@gmail.com and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestGateEntry(unittest.TestCase):
+	pass
